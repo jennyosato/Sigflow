a90a284fea02521971654e80a632efb756e02ffd
Jennifer <st.jennyandy@gmail.com>
2023-10-16 00:42:49 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Login from "./components/login";
 import Home from "./components/Home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AuthLayout from "./components/auth";
-import Sign from "./components/sign";
 
 import "./App.css";
 
 function App() {
   return (
     <>
+     
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="login" element={<Login />} />
+          <Route element={<AuthLayout/>}>
           <Route path="/" element={<Home />} />
-          <Route path="/sign" element={<Sign />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
     </>
